e7646b271c80b600e47fbe614cefcec95b286788
rexhinaIdobet <rexhina@idobet.com>
2018-10-04 16:28:40 +0200

    Add working version of GitHub Components!

---
+++ app/components/github/github-results.js
@@ -0,0 +1,7 @@
+(function (global) {
+
+    global.App.Controller('github-results', './components/github/', function ($scope, _update) {
+
+    });
+
+})(Function('return this')());
---
+++ app/components/github/github.js
@@ -0,0 +1,32 @@
+(function (global) {
+
+    global.App.Controller('github', './components/github/', function ($scope, _update) {
+        var GitHub = global.App.getModel('GitHub');
+        $scope.entities = GitHub.entities;
+        $scope.gitHubData = false;
+        $scope.entity = false;
+        $scope.term = false;
+
+        $scope.eventClicked = function () {
+            if ($scope.entity && $scope.term) {
+                GitHub.getGitHubData($scope.entity, {q: $scope.term, page:3}, function (gitHubData) {
+                    $scope.gitHubData = gitHubData;
+                    console.log('github:eventClicked', $scope.gitHubData);
+                    _update();
+                });
+            }
+        };
+
+        $scope.inputFocusout = function () {
+            console.log('github:inputFocusout', this.value);
+            $scope.term = this.value;
+            _update();
+        };
+
+        $scope.selectClicked = function (entity) {
+            console.log('github:selectClicked', entity);
+            $scope.entity = entity;
+            _update();
+        };
+    });
+})(Function('return this')());
