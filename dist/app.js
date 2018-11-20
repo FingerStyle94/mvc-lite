2d6fc61e435f11f308c8a14331fe9aaf825171ce
Aida <aida.dhima12@gmail.com>
2018-11-20 15:55:02 +0100

    add test controller that is used for calculating 2 numbers

--- dist/app.js
+++ dist/app.js
@@ -1,40 +1,100 @@
-(function(global) {
+(function (global) {
 
-    global.App.Controller('app', './', function($scope, _update) {
+    global.App.Controller('app', './', function ($scope, _update) {
         $scope.title = "App works!";
 
-        setTimeout(function() {
+        setTimeout(function () {
             $scope.title = "Hello World!";
             _update();
         }, 3000)
     });
 
 })(Function('return this')());
-(function(global) {
+(function (global) {
 
     var appRoutes = [
-        { path: '', redirect: 'home' },
-        { path: 'home', controller: 'main-layout' }
+        {path: '', redirect: 'home'},
+        {path: 'home', controller: 'main-layout'},
     ];
 
     new global.App.Router(appRoutes);
 
 })(Function('return this')());
-(function(global) {
+(function (global) {
 
-    global.App.Controller('main-layout', '/main-layout/', function($scope, _update) {
+    global.App.Controller('main-layout', '/main-layout/', function ($scope, _update) {
         $scope.title = "Main Layout";
 
-        $scope.buttonClicked = function(pageName) {
+        $scope.buttonClicked = function (pageName) {
             global.App.Router().navigateTo(pageName);
         };
     });
 
 })(Function('return this')());
-(function(global) {
+(function (global) {
 
-    global.App.Pipe('split', function(value, data) {
+    global.App.Pipe('split', function (value, data) {
         return value.split(data);
     });
 
+})(Function('return this')());
+(function (global) {
+
+    global.App.Model('AidaTest', function (setData) {
+
+        this.getCalMultiplication = function (n1, n2) {
+            var res = n1 * n2;
+            return res;
+        };
+
+        this.getCalDivision = function (n1, n2) {
+            var res = n1 / n2;
+            return res;
+        };
+
+    });
+
+})(Function('return this')());
+(function (global) {
+
+    global.App.Controller('aida-test', './components/aida-test/', function ($scope, _update) {
+        var NumberModel = global.App.getModel('AidaTest');
+        $scope.title = "Calculate Test";
+        $scope.number1 = '';
+        $scope.number2 = '';
+        $scope.result = null;
+
+        $scope.inputNumber = function () {
+            switch (this.name) {
+                case 'number1':
+                    $scope.number1 = this.value;
+                    break;
+                case 'number2':
+                    $scope.number2 = this.value;
+                    break;
+            }
+            _update();
+        };
+
+        $scope.calAddition = function () {
+            $scope.result = parseInt($scope.number1) + parseInt($scope.number2);
+            _update();
+        }
+
+        $scope.calSubtraction = function () {
+            $scope.result = parseInt($scope.number1) - parseInt($scope.number2);
+            _update();
+        }
+
+        $scope.calMultiplication = function () {
+            $scope.result = NumberModel.getCalMultiplication($scope.number1, $scope.number2);
+            _update();
+        }
+
+        $scope.calDivision = function () {
+            $scope.result = NumberModel.getCalDivision($scope.number1, $scope.number2);
+            _update();
+        }
+
+    });
 })(Function('return this')());
--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
 
 var LibraryConfig = {
     SOURCE: [
@@ -28,7 +29,13 @@ var AppConfig = {
 
 var DEST_PATH = "./dist/";
 
-gulp.task('compile-mvc', function() {
+var themePath = './app/style/';
+var SassConfig = {
+    SOURCE: [
+        themePath + '**/*.scss'
+    ]
+};
+gulp.task('compile-mvc', function () {
     return gulp.src(LibraryConfig.SOURCE)
         .pipe(concat(LibraryConfig.DEST_NAME + '.js'))
         .pipe(gulp.dest(DEST_PATH))
@@ -37,16 +44,23 @@ gulp.task('compile-mvc', function() {
         .pipe(gulp.dest(DEST_PATH));
 });
 
-gulp.task('compile-app', function() {
+gulp.task('compile-app', function () {
     return gulp.src(AppConfig.SOURCE)
         .pipe(concat(AppConfig.DEST_NAME + '.js'))
         .pipe(gulp.dest(DEST_PATH))
         .pipe(rename(AppConfig.DEST_NAME + '.min.js'))
-        .pipe(uglify())
         .pipe(gulp.dest(DEST_PATH));
 });
 
-gulp.task('watch', function() {
-    gulp.watch('js/**/*.js', ['compile-mvc']);
-    gulp.watch('app/**/*.js', ['compile-app']);
+gulp.task('sass', function () {
+    return gulp.src(SassConfig.SOURCE)
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename('main.css'))
+        .pipe(gulp.dest(DEST_PATH));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('js/**/*.js', gulp.series(['compile-mvc']));
+    gulp.watch('app/**/*.js', gulp.series(['compile-app']));
+    gulp.watch('app/style/**/*.scss', gulp.series(['sass']));
 });
